648b453ebacdec2124dcdeabfe32ed36f9e6849a
Jonathan Ahrens <ahrens.jm@gmail.com>
2022-12-17 16:32:24 -0800

    Advent of Code 2022

---
+++ Part1.ps1
@@ -0,0 +1,49 @@
+$ImportCommands = Get-Content 'F:\AdventOfCode\Day 7\Day7Input.txt'
+
+$CurrentFolder = ""
+$Folders = @{}
+foreach ($Command in $ImportCommands){
+$SplitCommand = $Command -split " "
+    If ($SplitCommand[0] -eq '$'){
+        switch($Command[1]){
+            "cd"{
+                switch($Command[2]){
+                   '/'{}
+                   '..'{
+                    $SplitFolders = $CurrentFodler -split "/"
+                    $CurrentFolder = ($SplitFolders[0..($SplitFolders.Length - 3)] -join "/") + "/"
+                } 
+                }
+            }
+            "ls"{
+                do{
+                    switch($SubCommand[0]){
+                        "dir"{
+                        
+                        }
+                    }
+                    default{
+                        
+                    }
+                }
+                until()
+            }
+            default {Write-Error "Error"}
+        }
+    }
+}
+
+
+
+
+
+
+
+1
+    1.1
+
+    1.2
+        1.2.1
+        1.2.2
+            1.2.2.1
+    1.3
