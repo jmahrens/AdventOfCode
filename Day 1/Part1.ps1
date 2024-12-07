814286be6d5509062ba66fca42db098bbf6e9df8
Jonathan Ahrens <ahrens.jm@gmail.com>
2024-12-07 08:42:24 -0800

    Moved files to 2022 directory

--- Day 1/Part1.ps1
+++
@@ -1,7 +0,0 @@
-$InputList = Get-content 'F:\AdventOfCode\Day 1\Day1Input.txt' -Raw 
-$Newline = [System.Environment]::Newline
-$Output = $inputlist -split "$Newline$Newline" | ForEach-Object {($_ -split $Newline | Measure-Object -Sum).Sum}| Sort-Object -Descending
-$Part1 = $Output[0]
-$Part2 = $Output[0]+$Output[1]+$Output[2]
-Write-Host $Part1
-write-host $Part2
